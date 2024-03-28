ea10e425067fde40175e42a8aa3375289d13773b
Bailey Brake <126217036+bbrake01@users.noreply.github.com>
2024-03-28 12:26:48 -0400

    Update traffic.c

--- Lab4/traffic.c
+++ Lab4/traffic.c
@@ -1,1 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/types.h>
+#include <linux/proc_fs.h>
+#include <linux/fcntl.h>
+#include <linux/uaccess.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
+#include <asm/uaccess.h>
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Kernelspace Module for BeagleBone Traffic Controller");
+
+static int major_number;
+static int char_dev_major=61;
 
